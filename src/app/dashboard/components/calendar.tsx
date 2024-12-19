1e4144c501d71feb37229a345abbd5013de1c016
Васютинский Глеб~ <g.v.vass@gmail.com>
2024-12-19 18:08:48 +0600

    #1 Resizable screen split using shadCN resizable

--- src/app/dashboard/components/calendar.tsx
+++ src/app/dashboard/components/calendar.tsx
@@ -12,18 +12,35 @@ export default function Calendar() {
   const [events, setEvents] = useState([
     { title: "event 1", id: "1" },
     { title: "event 2", id: "2" },
-    { title: "event 3", id: "3 " },
+    { title: "event 3", id: "3" },
     { title: "event 4", id: "4" },
     { title: "event 5", id: "5" },
   ]);
-
   const [allEvents, setAllEvents] = useState<Event[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [idToDelete, setIdToDelete] = useState<number | null>(null);
   const [newEvent, setNewEvent] = useState<Event>({
     title: "",
     start: "",
     allDay: false,
     id: 0,
   });
+
+  useEffect(() => {
+    let draggableEl = document.getElementById("draggable-el");
+    if (draggableEl) {
+      new FCDraggable(draggableEl, {
+        itemSelector: ".fc-event",
+        eventData: function (eventEl) {
+          let title = eventEl.getAttribute("title");
+          let id = eventEl.getAttribute("data");
+          let start = eventEl.getAttribute("start");
+          return { title, id, start };
+        },
+      });
+    }
+  }, []);
 
   function handleDateClick(arg: { date: Date; allDay: boolean }) {
     setNewEvent({
@@ -32,6 +49,7 @@ export default function Calendar() {
       allDay: arg.allDay,
       id: new Date().getTime(),
     });
+    setShowModal(true);
   }
 
   function addEvent(data: DropArg) {
@@ -43,50 +61,88 @@ export default function Calendar() {
       id: new Date().getTime(),
     };
     setAllEvents([...allEvents, event]);
-
-    const draggedEventTitle = data.draggedEl.title;
-    setEvents(events.filter((e) => e.title !== draggedEventTitle));
-    console.log(events.filter((e) => e.title !== draggedEventTitle));
   }
 
-  useEffect(() => {
-    let draggableEl = document.getElementById("draggable-el");
+  function handleDeleteModal(data: { event: { id: string } }) {
+    setShowDeleteModal(true);
+    setIdToDelete(Number(data.event.id));
+  }
 
-    if (draggableEl) {
-      new FCDraggable(draggableEl, {
-        itemSelector: ".fc-event",
-        eventData: function (eventEl) {
-          let title = eventEl.getAttribute("title");
-          let id = eventEl.getAttribute("data");
-          let start = eventEl.getAttribute("start");
+  function handleDelete() {
+    setAllEvents(
+      allEvents.filter((event) => Number(event.id) !== Number(idToDelete)),
+    );
+    setShowDeleteModal(false);
+    setIdToDelete(null);
+  }
 
-          return { title, id, start };
-        },
-      });
-    }
-  }, []);
+  function handleCloseModal() {
+    setShowModal(false);
+    setNewEvent({
+      title: "",
+      start: "",
+      allDay: false,
+      id: 0,
+    });
+    setShowDeleteModal(false);
+    setIdToDelete(null);
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewEvent({
+      ...newEvent,
+      title: e.target.value,
+    });
+  };
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setAllEvents([...allEvents, newEvent]);
+    setShowModal(false);
+    setNewEvent({
+      title: "",
+      start: "",
+      allDay: false,
+      id: 0,
+    });
+  }
 
   return (
-    <div className="fixed right-0 top-14 flex h-[calc(100vh-56px)] w-2/5 flex-col border-l border-r border-gray-300 p-2">
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-        headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "resourceTimelineWook, dayGridMonth,timeGridWeek",
-        }}
-        events={allEvents as EventSourceInput}
-        nowIndicator={true}
-        editable={true}
-        droppable={true}
-        selectable={true}
-        selectMirror={true}
-        dateClick={handleDateClick}
-        drop={(data) => addEvent(data)}
-        eventClick={(data) => console.log("event clicked, data:", data)}
-        eventDragStart={(info) => console.log("Drag start", info)}
-        height={"100%"}
-      />
+    <div className="">
+      <div className="col-span-8">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "resourceTimelineWook, dayGridMonth,timeGridWeek",
+          }}
+          events={allEvents as EventSourceInput}
+          nowIndicator={true}
+          editable={true}
+          droppable={true}
+          selectable={true}
+          selectMirror={true}
+          dateClick={handleDateClick}
+          drop={(data) => addEvent(data)}
+          eventClick={(data) => console.log("delete", data)}
+        />
+      </div>
+      <div
+        id="draggable-el"
+        className="ml-8  border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50"
+      >
+        <h1 className="font-bold text-lg text-center">Drag Event</h1>
+        {events.map((event) => (
+          <div
+            className="fc-event border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white"
+            title={event.title}
+            key={event.id}
+          >
+            {event.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,43 +8,61 @@ import { useAppSelector } from "@/lib/hooks";
 import ToDoListCreator from "./components/to-do-list-creator";
 import Calendar from "./components/calendar";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 export default function DashboardPage() {
   const tasks = useAppSelector((state) => state.tasks);
   const lists = useAppSelector((state) => state.lists);
 
   return (
-    <>
-      <div className="mt-[56px] flex h-screen w-3/5 flex-col overflow-scroll bg-gradient-to-tr from-red-300 to-indigo-300">
-        <div className="bg-white w-full h-24 border-b border-gray-300">
-          <h6>Project Name</h6>
-        </div>
-        <div className="h-full w-full p-2">
-          <DragDropContext
-            onDragEnd={({ destination, source }) => {
-              if (!destination) {
-                return;
-              }
-              reorderTasks(lists, source, destination);
-            }}
-          >
-            <div className="flex h-full gap-3">
-              {Object.entries(lists).map(([listID, listBody], index) => (
-                <ToDoList
-                  listID={listID}
-                  listName={listBody.name}
-                  listTasks={listBody.taskIDs.map((taskId) => tasks[taskId])}
-                  key={index}
-                />
-              ))}
-
-              <ToDoListCreator />
+    <div className="h-screen mt-[54px]">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="h-screen  w-full rounded-lg border "
+      >
+        <ResizablePanel defaultSize={65} minSize={50}>
+          <div className="flex h-screen flex-col overflow-scroll bg-gradient-to-tr from-red-300 to-indigo-300">
+            <div className="bg-white w-full h-24 border-b border-gray-300">
+              <h6>Project Name</h6>
             </div>
-          </DragDropContext>
-        </div>
-      </div>
-      <div className="w-2/5">
-        <Calendar />
-      </div>
-    </>
+            <div className="h-full w-full p-2">
+              <DragDropContext
+                onDragEnd={({ destination, source }) => {
+                  if (!destination) {
+                    return;
+                  }
+                  reorderTasks(lists, source, destination);
+                }}
+              >
+                <div className="flex h-full gap-3">
+                  {Object.entries(lists).map(([listID, listBody], index) => (
+                    <ToDoList
+                      listID={listID}
+                      listName={listBody.name}
+                      listTasks={listBody.taskIDs.map(
+                        (taskId) => tasks[taskId],
+                      )}
+                      key={index}
+                    />
+                  ))}
+
+                  <ToDoListCreator />
+                </div>
+              </DragDropContext>
+            </div>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={35} minSize={35}>
+          <div className="flex h-full items-center justify-center p-6">
+            <Calendar />
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
   );
 }
