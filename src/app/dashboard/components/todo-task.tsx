2bf450dd74a9682599b9fe749efd22a863d4fa80
Васютинский Глеб~ <g.v.vass@gmail.com>
2025-05-05 22:05:12 +0500

    terrible paths

--- src/app/dashboard/components/todo-task.tsx
+++ src/app/dashboard/components/todo-task.tsx
@@ -1,7 +1,7 @@
 "use client";
-import ActivatedInput from "@/components/ui/activated-input";
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
+import ActivatedInput from "../../../components/ui/activated-input";
+import { Button } from "../../../components/ui/button";
+import { Checkbox } from "../../../components/ui/checkbox";
 import {
   DropdownMenu,
   DropdownMenuContent,
