47e72b0019b341ccdcecb67f0e0e790d2c943f64
Васютинский Глеб~ <g.v.vass@gmail.com>
2025-05-05 21:52:11 +0500

    config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from "url";
 import path from "path";
+
+// ES Modules equivalent for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig = {
   webpack: (config) => {
